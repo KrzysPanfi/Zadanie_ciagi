b0913e32c6e67fab108cbfdf83775c1317102088
Krzysztof <krzyspanfilewicz@gmail.com>
2024-03-07 15:27:54 +0100

    Robienie zad2

--- src/Main.java
+++ src/Main.java
@@ -6,28 +6,63 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-       String filepath="Downloads:binarne.txt";
+       String filepath="Z:binarne.txt";
        ArrayList<String> lista_binarna=Wczytaj_dane(filepath);
-
+       for(String i:Zad1(lista_binarna)){
+           System.out.println(i);
+       }
+        for(int i:Zad2(lista_binarna)){
+            System.out.println(Integer.toString(i));
+        }
 
     }
-    private static String Zad1(ArrayList<String>lista_binarna){
-        ArrayList<String>Wynik=new ArrayList<String>()
+    private static ArrayList<String> Zad1(ArrayList<String>lista_binarna){
+        ArrayList<String>Wynik=new ArrayList<String>();
         int najdluszy_dlugosc=0;
         String najdluszy="";
         int count=0;
+        String pierwszycykl;
+        String drugicykl;
         for(String i:lista_binarna){
-            for(int j=0;j<i.length()/2;j++){
-                if(i.charAt(j)==i.charAt(j+i.length()/2)){
+            pierwszycykl=i.substring(0,i.length()/2);
+            drugicykl=i.substring(i.length()/2);
+           if(pierwszycykl.equals(drugicykl)){
+               count++;
+               if(i.length()>najdluszy_dlugosc){
+                   najdluszy_dlugosc=i.length();
+                   najdluszy=i;
+               }
+
+
+                    }
+                }
+        Wynik.add(Integer.toString(count));
+        Wynik.add(Integer.toString(najdluszy_dlugosc));
+        Wynik.add(najdluszy);
+        return Wynik;
+            }
+
+
+private static ArrayList<Integer> Zad2(ArrayList<String> lista_binarna){
+        ArrayList<Integer>Wynik=new ArrayList<Integer>();
+        int count=0;
+        int najkrotsza_niepoprawna=0;
+        for(String i:lista_binarna){
+            String[]arr1=i.split("(?<=\\G.{4})");
+            for(String num:arr1){
+                if(Integer.parseInt(num)>1001){
                     count++;
-                    if(i.length()>najdluszy_dlugosc){
-                        najdluszy=i;
-                        najdluszy_dlugosc=i.length();
+                    if(najkrotsza_niepoprawna>i.length()){
+                        najkrotsza_niepoprawna=num.length();
                     }
                 }
             }
-        }return najdluszy;
-    }
+        }
+        Wynik.add(count);
+        Wynik.add(najkrotsza_niepoprawna);
+        return Wynik;
+}
+
     private static ArrayList<String> Wczytaj_dane(String filePath) {
         ArrayList<String> lista_slow=new ArrayList<String>();
         try {
